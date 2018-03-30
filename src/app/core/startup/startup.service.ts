f0a018a26593cafa44d8f275f583bf9f62fb0e6c
LZHD <lzhdfxq@163.com>
2018-03-30 18:16:30 +0800

    基础数据获取

--- src/app/core/startup/startup.service.ts
+++ src/app/core/startup/startup.service.ts
@@ -1,4 +1,5 @@
 import { Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 
 /**
@@ -7,7 +8,19 @@ import { Injectable} from '@angular/core';
  */
 @Injectable()
 export class StartupService {
-    constructor() { }
-    load() {
+    constructor(private http: HttpClient) { }
+	load(): Promise<any> {
+		// only works with promises
+		// https://github.com/angular/angular/issues/15088
+		return new Promise((resolve, reject) => {
+			this.http.get('assets/config.json', {responseType: 'json'})
+				.subscribe((data) => {
+					console.log(data);
+				},
+				() => { },
+				() => {
+					resolve(null);
+				});
+		});
 	}
 }
