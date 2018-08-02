f223616df8a1cc43b60ab20de0a49110a7d54008
LZHD <lzhdfxq@163.com>
2018-08-02 20:04:06 +0800

    修改

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
-import {SharedModule} from "./shared/shared.module";
-import {RoutesModule} from "./routes/routes.module";
-import {LayoutModule} from "./layout/layout.module";
-import {ToastModule} from "./shared/toast/toast.module";
-import {StartupService, StartupServiceFactory} from "./core/startup/startup.service";
-import {CoreModule} from "./core/core.module";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {DefaultInterceptor} from "./core/net/default-interceptor";
+import {SharedModule} from './shared/shared.module';
+import {RoutesModule} from './routes/routes.module';
+import {LayoutModule} from './layout/layout.module';
+import {ToastModule} from './shared/toast/toast.module';
+import {StartupService, StartupServiceFactory} from './core/startup/startup.service';
+import {CoreModule} from './core/core.module';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {DefaultInterceptor} from './core/net/default-interceptor';
 import {SidebarService} from './layout/sidebar/service/sidebar.service';
-import {JasperoConfirmationsModule} from "@jaspero/ng-confirmations";
-import {NgZorroAntdModule, NZ_I18N, zh_CN} from "ng-zorro-antd";
-import {registerLocaleData} from "@angular/common";
+import {JasperoConfirmationsModule} from '@jaspero/ng-confirmations';
+import {NZ_I18N, zh_CN} from 'ng-zorro-antd';
+import {registerLocaleData} from '@angular/common';
 import zh from '@angular/common/locales/zh';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
 
 registerLocaleData(zh);
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,31 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-// 自己
-import {GridBaseModule} from './grid-base/grid-base.module';
-import {ToastModule} from './toast/toast.module';
-import {TreeBaseModule} from './tree-base/tree-base.module';
-import {MapModule} from './map/map.module';
 // 第三方
 import {AgGridModule} from 'ag-grid-angular';
 import {SidebarModule} from 'ng-sidebar';
 import {SuiModule} from 'ng2-semantic-ui';
 import {NgZorroAntdModule} from 'ng-zorro-antd';
+// 自己
+import {GridBaseModule} from './grid-base/grid-base.module';
+import {TreeBaseModule} from './tree-base/tree-base.module';
+import {ToastModule} from './toast/toast.module';
+import {MapModule} from './map/map.module';
+
+const THIRDMODULES = [
+	AgGridModule,
+	SidebarModule,
+	SuiModule,
+	NgZorroAntdModule
+];
+const MYMODULES = [
+	GridBaseModule,
+	ToastModule,
+	TreeBaseModule,
+	MapModule
+];
+const COMPONENTS = [];
+const DIRECTIVES = [];
 
 /**
  * 公共模块
@@ -22,25 +37,19 @@ import {NgZorroAntdModule} from 'ng-zorro-antd';
 		FormsModule,
 		RouterModule,
 		ReactiveFormsModule,
-		NgZorroAntdModule
+		...THIRDMODULES
 	],
-	declarations: [],
-	providers: [],
-	entryComponents: [],
+	declarations: [
+		...COMPONENTS,
+		...DIRECTIVES
+	],
 	exports: [
 		CommonModule,
 		FormsModule,
 		ReactiveFormsModule,
 		RouterModule,
-		SuiModule,
-		SidebarModule,
-		AgGridModule,
-		GridBaseModule,
-		ToastModule,
-		TreeBaseModule,
-		MapModule,
-		NgZorroAntdModule
+		...THIRDMODULES,
+		...MYMODULES
 	]
 })
-export class SharedModule {
-}
+export class SharedModule {}
